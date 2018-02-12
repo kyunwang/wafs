31c9d77012f4a9f2fd14fa7707a234c29be3ff57
Kangyun Wang <wky@250.185.028.145.hva.nl>
2018-02-12 13:00:37 +0100

    Basic api call and storing in localstorage

---
+++ app/README.md
@@ -0,0 +1,4 @@
+Challenges:
+- Use vanilla Javascript only
+- Try to use CSS Grid as main layout tool
+- Microlibs may be used for templating and routing
--- app/public/scripts/app.js
+++ app/public/scripts/app.js
@@ -8,9 +8,23 @@
 
 	const app = {
 		init: async function() {
-			console.log("Initializing app");
-			configs.allRoutes = await helpers.getElements('section'); // Needs a better way for scalability (screen/route class?)
+			console.log('Initializing app');
+			let initialData;
+
+			// Set our initial routes and animedata in a promise
+			[configs.allRoutes, initialData] = await Promise.all([
+				helpers.getElements('section'),
+				api.get('anime', 20)
+			]);
+
 			routes.init();
+
+
+			// api.getAnime(20);
+			// api.get('anime', 20);
+			// Setting our anime data in localstorage
+			helpers.setData('animeData', helpers.stringify(initialData));
+			console.log(helpers.getData('animeData'))
 		}
 	};
 
@@ -45,11 +59,43 @@
 		}
 	};
 
+	// Max limit for the api is 20
 	const api = {
 		baseUrl: 'https://kitsu.io/api/edge',
-		get: {
-			// return fetch()
+		baseHeader: {
+			'Accept': 'application/vnd.api+json',
+			'Content-Type': 'application/vnd.api+json'
+		},
+		get: async function(route = '', limit = 10, offset = 0) {
+			const data = await fetch(`${this.baseUrl}/${route}?page[limit]=${limit}&page[offset]=${offset}`, {
+				headers: this.baseHeader
+			})
+			.then((res, err) => res.json())
+			.catch(err => debug.error(err));
+
+			console.log(data)
+			return data;
+		},
+
+		getAnime: async function(limit = 10, offset = 0) {
+			const data = await fetch(`${this.baseUrl}/anime?page[limit]=${limit}&page[offset]=${offset}`, {
+				headers: this.baseHeader
+			})
+			.then((res, err) => res.json())
+			.then((res, err) => {
+				helpers.setData('animeData', helpers.stringify(res));
+				console.log(res.data.length)
+				return res;
+			})
+			.catch(err => debug.error(err));
+
+			return data;
 		}
+	}
+
+	const storage = {
+		getData() {},
+		storeData() {}
 	}
 
 	const helpers = {
@@ -57,8 +103,17 @@
 		getElement(element) { return document.querySelector(element); },
 		getElements(element) { return document.querySelectorAll(element); },
 		// getAllRoutes () { return helpers.getElements('section') },
-		shortenString(text, start, end) { return end ? text.substr(start, end) : text.substr(start); }
+		shortenString(text, start, end) { return end ? text.substr(start, end) : text.substr(start); },
+		setData(key, data) { return localStorage.setItem(key, data); },
+		getData(key) { return localStorage.getItem(key); },
+		deleteData(key) { return localStorage.removeItem(key); },
+		stringify(data) { return JSON.stringify(data); },
+		parse(data) { return JSON.parse(data); },
 	};
+
+	const debug = {
+		error(err) { console.log('Oops a error: ', err); return err; }
+	}
 
 	app.init();
 })();
